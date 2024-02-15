d0b241e6058f4b129c64ba09d62b8f3efda52784
Exkalamity <kyledereklam@gmail.com>
2024-02-14 19:59:39 -0800

    Successfully implemented the probabilities. Probably correct

--- characters/qingque.py
+++ characters/qingque.py
@@ -28,116 +28,15 @@ class Qingque(Character):
     self.success_table = make_success_table()
     self.state_table = make_state_table()
     self.sp_table = make_sp_table()
+    self.success_vector = None
     self.success_chance = None
     self.likely_multiplier = None
     self.average_multiplier = None
     
-  def basic(self, verbose = None):
-    arena = self.arena
-    if verbose is None:
-      verbose = self.verbose
-    arena.sp += 1
-    self.energy += 20
-    if verbose:
-      print("QQ uses basic! SP +1")
-    mv = 1.1
-    avdmg = self.average_damage(mv)
-    self.autarkey_multiplier = min(2, 1 + self.autarkey_chance*self.skill_uses)
-    self.expected_damage = avdmg*self.autarkey_multiplier
-    critdmg = self.max_damage(mv)
-    self.maximum_damage = 2 * critdmg
-
-  def enhanced_basic(self, verbose= None):
-    if verbose is None:
-      verbose = self.verbose
-    #add movement values
-    #add 1 to SP if E6
-    self.energy += 20
-    if self.eidolon >= 6:
-      self.arena.sp +=1
-    if verbose:
-      print("QQ uses enhanced basic! SP +1")
-    self.tiles = 0
-    self.energy += 20
-    mv = 4.84
-    avdmg = self.average_damage(mv)
-    self.autarkey_multiplier = min(2, 1 + self.autarkey_chance*self.skill_uses)
-    self.expected_damage = avdmg*self.autarkey_multiplier
-    critdmg = self.max_damage(mv)
-    self.maximum_damage = 2 * critdmg
-
-  def skill(self, verbose = None):
-    arena = self.arena
-    if verbose is None:
-      verbose = self.verbose
-    #Draw tiles
-    if self.tile_battle:
-      arena.sp +=1
-      self.tile_battle = False
-    if verbose:
-      print(f"Attempting to draw {self.hidden_hand - self.tiles} tiles with {arena.sp} SP")
-    self.draws = np.ceil((self.hidden_hand - self.tiles)/2)
-    self.energy += self.draws
-  
-  def hidden_hand_failure(self, verbose = None):
-    arena = self.arena
-    if verbose is None:
-      verbose = self.verbose
-    dmg_bonus = 0.408*arena.sp
-    self.skill_uses = arena.sp
-    sp_used = arena.sp
-    self.damage_percent += dmg_bonus
-    self.buffs_list.append({"attribute name":"damage_percent", "recipient":self, "tickdown":self, "value":dmg_bonus, "duration":1})
-    arena.sp = 0
-    if verbose:
-      print("Hidden Hand Failed!")
-      print(f"Attempted to use {sp_used} SP to draw {self.draws * 2} tiles")
-      print(f"Damage increased by {dmg_bonus * 100}%")
-  
-  def hidden_hand_success(self,verbose = None):
-    arena = self.arena
-    if verbose is None:
-      verbose = self.verbose
-    atk_bonus = 0.792
-    dmg_bonus = 0.408*self.draws
-    self.skill_uses = self.draws
-    self.attack_percent += atk_bonus
-    self.damage_percent += dmg_bonus
-    self.buffs_list.append({"attribute name":"attack_percent", "recipient":self, "tickdown":self, "value":atk_bonus, "duration":1})
-    self.buffs_list.append({"attribute name":"damage_percent", "recipient":self, "tickdown":self, "value":dmg_bonus, "duration":1})
-    arena.sp -= self.draws
-    self.update_stats()
-    if verbose:
-      print("Hidden Hand Sucess!")
-      print(f"{self.draws} SP consumed to draw {self.draws * 2} tiles")
-      print(f"Attack increased by {atk_bonus * 100}%, Damage increased by {dmg_bonus * 100}%")
-      self.get_stats()
-  
-  def ult(self, verbose = None):
-    if verbose is None:
-      verbose = self.verbose
-    mv = 5*2.16
-    self.ult_expected_damage = self.average_damage(mv)
-    self.ult_max_damage = self.max_damage(mv)
-    if verbose:
-      print("Qingque uses ultimate!")
-    self.energy = 5
-
-  def update_stats(self, verbose = None):
-    if verbose is None:
-      verbose = self.verbose
-    try:
-      self.total_attack = self.base_attack * (1 + self.attack_percent) + self.flat_attack
-    except TypeError:
-      if verbose:
-        print("Warning: Qingque's update_stats failed due to blank stat values.")
-      else:
-        pass
-
   def check_hh(self, sp = None, tiles = None, threshold = None):
     arena = self.arena
     if sp is None:
-      sp = arena.sp
+      sp = int(arena.sp)
     if tiles is None:
       tiles = self.tiles
     if threshold is None:
@@ -162,7 +61,8 @@ class Qingque(Character):
       average_SP *= state_vector.copy()
       #print(average_SP)
       success_vector *= state_vector.copy()
-      print(success_vector)
+      self.success_vector = success_vector.copy()
+      #print(success_vector)
       self.success_chance = 1-sum(fail_vector)
       self.average_multiplier = sum(average_SP)
       if self.success_chance > threshold:
@@ -171,3 +71,107 @@ class Qingque(Character):
       else:
         self.likely_multiplier = 0
         self.hidden_hand = False
+    
+  # def basic(self, verbose = None):
+  #   arena = self.arena
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   arena.sp += 1
+  #   self.energy += 20
+  #   if verbose:
+  #     print("QQ uses basic! SP +1")
+  #   mv = 1.1
+  #   avdmg = self.average_damage(mv)
+  #   self.autarkey_multiplier = min(2, 1 + self.autarkey_chance*self.skill_uses)
+  #   self.expected_damage = avdmg*self.autarkey_multiplier
+  #   critdmg = self.max_damage(mv)
+  #   self.maximum_damage = 2 * critdmg
+
+  # def enhanced_basic(self, verbose= None):
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   #add movement values
+  #   #add 1 to SP if E6
+  #   self.energy += 20
+  #   if self.eidolon >= 6:
+  #     self.arena.sp +=1
+  #   if verbose:
+  #     print("QQ uses enhanced basic! SP +1")
+  #   self.tiles = 0
+  #   self.energy += 20
+  #   mv = 4.84
+  #   avdmg = self.average_damage(mv)
+  #   self.autarkey_multiplier = min(2, 1 + self.autarkey_chance*self.skill_uses)
+  #   self.expected_damage = avdmg*self.autarkey_multiplier
+  #   critdmg = self.max_damage(mv)
+  #   self.maximum_damage = 2 * critdmg
+
+  # def skill(self, verbose = None):
+  #   arena = self.arena
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   #Draw tiles
+  #   if self.tile_battle:
+  #     arena.sp +=1
+  #     self.tile_battle = False
+  #   if verbose:
+  #     print(f"Attempting to draw {self.hidden_hand - self.tiles} tiles with {arena.sp} SP")
+  #   self.draws = np.ceil((self.hidden_hand - self.tiles)/2)
+  #   self.energy += self.draws
+  
+  # def hidden_hand_failure(self, verbose = None):
+  #   arena = self.arena
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   dmg_bonus = 0.408*arena.sp
+  #   self.skill_uses = arena.sp
+  #   sp_used = arena.sp
+  #   self.damage_percent += dmg_bonus
+  #   self.buffs_list.append({"attribute name":"damage_percent", "recipient":self, "tickdown":self, "value":dmg_bonus, "duration":1})
+  #   arena.sp = 0
+  #   if verbose:
+  #     print("Hidden Hand Failed!")
+  #     print(f"Attempted to use {sp_used} SP to draw {self.draws * 2} tiles")
+  #     print(f"Damage increased by {dmg_bonus * 100}%")
+  
+  # def hidden_hand_success(self,verbose = None):
+  #   arena = self.arena
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   atk_bonus = 0.792
+  #   dmg_bonus = 0.408*self.draws
+  #   self.skill_uses = self.draws
+  #   self.attack_percent += atk_bonus
+  #   self.damage_percent += dmg_bonus
+  #   self.buffs_list.append({"attribute name":"attack_percent", "recipient":self, "tickdown":self, "value":atk_bonus, "duration":1})
+  #   self.buffs_list.append({"attribute name":"damage_percent", "recipient":self, "tickdown":self, "value":dmg_bonus, "duration":1})
+  #   arena.sp -= self.draws
+  #   self.update_stats()
+  #   if verbose:
+  #     print("Hidden Hand Sucess!")
+  #     print(f"{self.draws} SP consumed to draw {self.draws * 2} tiles")
+  #     print(f"Attack increased by {atk_bonus * 100}%, Damage increased by {dmg_bonus * 100}%")
+  #     self.get_stats()
+  
+  # def ult(self, verbose = None):
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   mv = 5*2.16
+  #   self.ult_expected_damage = self.average_damage(mv)
+  #   self.ult_max_damage = self.max_damage(mv)
+  #   if verbose:
+  #     print("Qingque uses ultimate!")
+  #   self.energy = 5
+
+  # def update_stats(self, verbose = None):
+  #   if verbose is None:
+  #     verbose = self.verbose
+  #   try:
+  #     self.total_attack = self.base_attack * (1 + self.attack_percent) + self.flat_attack
+  #   except TypeError:
+  #     if verbose:
+  #       print("Warning: Qingque's update_stats failed due to blank stat values.")
+  #     else:
+  #       pass
+
+  
--- utils/data_table.py
+++ utils/data_table.py
@@ -12,9 +12,18 @@ class Action_Table(Data_Table):
         self.df = pd.DataFrame(columns = ["Name", "Cycle", "Action Gauge", "Current SP", "Current Energy", "Action", "SP Gain"])
         self.sp_now = None
 
-    def append(self, arena, chara, action, sp_change):
+    def add_row(self, arena, chara, action, sp_change):
         new_row = {"Name":chara.name, "Cycle":arena.cycle, "Action Gauge":arena.action_gauge, "Current SP":arena.sp, 
-                   "Current Energy":chara.energy, "Action":action}
-        self.df = self.df.append(new_row, ignore_index = True)
+                   "Current Energy":chara.energy, "Action":action, "SP Gain":sp_change}
+        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index = True)
     # def update_sp(self, arena):
     #     self.sp_now = arena.sp
+class Qingque_Probability_Table(Data_Table):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.df = pd.DataFrame(columns = ["Cycle", "Action Gauge", "Current SP", "Current Tiles", "P(Success)", "Average # Skills", "Most Likely # Skills", "SP Consumed"])
+    def add_row(self, arena, chara, tiles):
+        new_row ={"Cycle":arena.cycle, "Action Gauge":arena.action_gauge, "Current SP":arena.sp, "Current Tiles":tiles, "P(Success)":chara.success_chance, 
+                    "Average # Skills":chara.average_multiplier, "Most Likely # Skills":chara.likely_multiplier, "SP Consumed":np.round(chara.likely_multiplier)}
+        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index = True)
+    
